921b69e28ec4ee4de235d359396b19c1945f1950
Salih Algül <83082628+salihalgul@users.noreply.github.com>
2022-08-21 22:52:50 +0300

    Add files via upload

---
+++ 1_ilk_program.py
@@ -0,0 +1,34 @@
+print("Merhaba Dünya")
+
+# print('Bu benim ilk programım :))')
+
+'''
+burada ilk uygulamamızı yapıyoruz
+'''
+
+"""
+Diğer programcı arkadaşlara notumuzdur.
+"""
+
+# Aritmetik işlemler
+
+# Toplama +
+# Çıkarma -
+# Çarpma *
+# Bölme /
+# Üstel **
+# Mod %
+# Tam sayılı bölme(bölüm) //
+
+a = 24
+b = 16
+c = 5
+d = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(c ** d)
+print(c % d)
+print(c // d)
+print(a // d)
---
+++ 2_Degiskenler.py
@@ -0,0 +1,36 @@
+"""
+Değişkenler bir atıftır.
+Bilgisayarın hafızasında, o değere ait adresin ismidir. (adres numarası)
+Atama (assignment) işlemi, yeni bir değişken oluşturur ve ona bir değer atar.
+"""
+# a = 12
+# print(a)
+#
+# bilgi = "Python adı, Monthy Python ve Kutsal Kase dizisinden gelir."
+# print(bilgi)
+#
+# pi = 3.1415
+# print(pi)
+#
+# # snake_case_isimlendirme
+#
+# bir_sayi = 5
+# print(bir_sayi)
+#
+# ortalama_hiz = 60
+# print(ortalama_hiz)
+#
+# onluk = 10
+# print(onluk)
+#
+# four = 4
+# print(four)
+
+deger = False
+Sayı = 45
+print(deger)
+print(type(deger))
+
+liste = (1, 2, 3, 'Python', 'True', 3.5)
+print(liste)
+print(type(liste))
